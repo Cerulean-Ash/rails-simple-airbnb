442830142df2bf0b74cda9060b4fbbd34ac144b6
Ashley Lampard <ashleylampard@live.com>
2022-04-28 11:52:41 +0100

    added destroy functionality

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -12,7 +12,7 @@ class FlatsController < ApplicationController
   end
 
   def create
-    @flat = Flat.new(params[:flat])
+    @flat = Flat.new(flat_params)
     if @flat.save
       redirect_to flat_path(@flat)
     else
@@ -28,6 +28,12 @@ class FlatsController < ApplicationController
     @flat = Flat.find(params[:id])
     @flat.update(flat_params)
     redirect_to flat_path(@flat)
+  end
+
+  def destroy
+    @flat = Flat.find(params[:id])
+    @flat.destroy
+    redirect_to flats_path
   end
 
   private
